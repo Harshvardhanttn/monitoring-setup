5276b8505a9dc5c45756c85f6ab114351851c9f0
Harshit-992 <85869265+Harshit-992@users.noreply.github.com>
2023-06-06 15:36:34 +0530

    Updated script2.sh

--- script2.sh
+++ script2.sh
@@ -2,7 +2,7 @@
 
 # Prompt the user for the path to the text file
 read -p "Enter name: " name
-read -p "Enter path to the text file: " file_path
+file_path='keyvalue.txt'
 var=""
 # Read key-value pairs from the text file
 while IFS='=' read -r key value; do
